87f22cdf44a375a2cd2436ac9adec5ac13ab4855
Juan Pablo Villaplana Corrales <jpvillaplana@gmail.com>
2026-01-20 12:44:36 -0600

    Configure custom domain pablovillaplana.me

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pablovillaplana.github.io',
-  base: '/pablovillaplana.me',
+  site: 'https://pablovillaplana.me',
   vite: {
     plugins: [tailwindcss()]
   },
